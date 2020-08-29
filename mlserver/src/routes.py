8de9d39bac66cc452e377d0708ef2ad29c02b4a2
opeco17 <opeco17@gmail.com>
2020-08-30 01:02:27 +0900

    MLServerのdocstring整備

--- mlserver/src/routes.py
+++ mlserver/src/routes.py
@@ -17,6 +17,14 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    """作品詳細の特徴量を元にポイント予測を行うAPI
+
+    Fields:
+        all_features_df: 作品詳細の全特徴量を保持したDataFrame
+        features_df: ポイント予測に使用する特徴量を保持したDataFrame
+        predicted_point: 作品のポイント予測結果 (0: 評価されない 1: 評価される)
+    """
+
     response = {
         'success': False,
         'Content-Type': 'application/json'
@@ -24,15 +32,10 @@ def predict():
 
     if request.method == 'POST':
         if request.get_json():
-            '''
-            json all_features: pair of columns and value of list
-            pd.DataFrame fetures: features of narou novel
-            np.ndarray predicted_point: point of narou novel predicted by machine learning
-            '''
             all_features = request.get_json()
-            all_features = pd.DataFrame(json.loads(all_features))
-            features = all_features[feature_names]
-            predicted_point = model.predict(features)
+            all_features_df = pd.DataFrame(json.loads(all_features))
+            features_df = all_features_df[feature_names]
+            predicted_point = model.predict(features_df)
             response['prediction'] = predicted_point.tolist()
             response['success'] = True
     return jsonify(response)
